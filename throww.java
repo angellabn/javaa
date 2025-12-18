c7dd704dcf8d6e06d5cdf56ce728977b43cca611
angellabn <angellabn77@gmail.com>
2025-12-18 18:23:28 +0530

    njhi

--- throww.java
+++ throww.java
@@ -2,6 +2,14 @@
 
 public class throww {
     public static void main(String[] args) {
-        
+        double amount=3000;
+        double balance=5000;
+
+        if(amount<balance){
+            throw new IllegalArgumentException("insufficient balance");
+        }
+
+        balance-=amount;
+        System.out.println("withdrawl successful. remaining balance"+ balance);
     }
 }
